a13e1ed62c35493bda0ce8681e56098c23eb36a9
Kim Pang <pang921008@gmail.com>
2024-04-07 23:11:39 +0800

    always use resttemplate to call the other services 's api before use microservices.

--- user/src/main/java/com/eureka/user/controller/UserController.java
+++ user/src/main/java/com/eureka/user/controller/UserController.java
@@ -2,6 +2,9 @@ package com.eureka.user.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,10 +12,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/testuser")
     public String testUser() {
         return "testUser";
+    }
+    
+    @GetMapping("/getapifromorder")
+    public String getMethodName() {
+        // this is a method to get data from order before use eureka.
+        String response = restTemplate.getForObject("http://localhost:8082/order/testorder", String.class);
+        return response;
     }
     
 }
